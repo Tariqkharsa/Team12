743886e42bda2b8ee8fdf5c090dba8a8b667ae22
stella <yinglan165@gmail.com>
2022-03-30 13:27:44 -0500

    add exhibition_page.php stella_style.css

---
+++ WebTeam12/Web Files/exhibition_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset = "utf-8">
+        <title>Museum Exhibition</title>
+        <link rel="stylesheet" href = "./stella_style.css">
+        <meta name="viewpoint" conten="width=device-width, initial-scale=1">
+
+    </head>
+
+    <body>
+        <div class="header-container">
+            <a href= "../index.php"><img class= "logo" src="../images/museum_icon3.png" alt="logo"></a>
+            <nav>
+                <ul class= "nav-links">
+                <li class="nav-item name"><a href="../index.php">FabMuseum</a></li>
+                    <li class="nav-item"><a href=#> About</a></li>
+                    <li class="nav-item"><a href= "./Ticket.html"> Tickets</a></li>
+                    <li class="nav-item"><a href= "./exhibition_page.php">Exhibition</a></li>
+                    <li class="nav-item"><a href=#> Collection </a></li>
+                    
+                    <li class="nav-item login-button"><a href=#><button>Login</button></a></li>
+                </ul>
+            </nav>
+        </div>
+    </body>
+</html>
