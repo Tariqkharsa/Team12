5be37aaf7e3f01bad2514be00533ae2785901fec
C-Sandoval <68603806+C-Sandoval@users.noreply.github.com>
2022-03-29 20:27:01 -0500

    Files for ticket homepages

---
+++ WebTeam12/Web Files/database.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+$dbServername = "team12.copftkcel1k2.us-east-1.rds.amazonaws.com";
+$dbUser = "admin";
+$dbPass = "Group12,museum";
+$dbName = "FinalTeam12";
+
+$connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName) or die("Unable to Connect to '$dbServername'");
+// mysqli_select_db($connect, $dbName) or die("Could not open the db '$dbName'");
+if(mysqli_connect_errno()) {
+    echo "Failed to connect to mySQL database: " mysqli_connect_error();
+    die('Bad connection'. $connect->connect_error);
+}
+
+?>
