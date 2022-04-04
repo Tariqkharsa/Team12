955e50971bf2bcde40aefcfdd5e9ddb1050a4d15
Nicholas Hoang <nhhoang3@CougarNet.UH.EDU>
2022-04-04 15:56:22 -0500

    added gift shop folder

---
+++ GiftWebApp/Data/Migrations/20220330230906_initalsetup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GiftWebApp.Data.Migrations
+{
+    public partial class initalsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GiftShop",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    ItemName = table.Column<string>(nullable: true),
+                    Price = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GiftShop", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GiftShop");
+        }
+    }
+}
---
+++ GiftWebApp/Data/Migrations/20220331014251_setup3.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GiftWebApp.Data.Migrations
+{
+    public partial class setup3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ItemId",
+                table: "GiftShop",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "GiftShop");
+        }
+    }
+}
